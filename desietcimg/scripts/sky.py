fb98552e14f7ea5f658504283cbfb2e7f652044d
David Kirkby <dkirkby@uci.edu>
2019-09-07 10:09:32 -0700

    Implement option to simulate multiple exposures

--- desietcimg/scripts/sky.py
+++ desietcimg/scripts/sky.py
@@ -19,6 +19,8 @@ def simulate():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', action='store_true',
         help='provide verbose output on progress')
+    parser.add_argument('-n', '--nstudy', type=int, default=1, metavar='N',
+        help='number of exposures to simulate')
     parser.add_argument('--exptime', type=float, default=60,
         help='exposure time in seconds to simulate')
     parser.add_argument('--max-rate', type=float, default=150,
@@ -37,6 +39,8 @@ def simulate():
         help='simulate pixel defects')
     parser.add_argument('--saveplot', type=str, default='simskycam.png',
         help='filename where plot is saved')
+    parser.add_argument('--outname', type=str, default='simskycam.csv',
+        help='name of CSV file where results are saved when nstudy > 1')
     parser.add_argument('--seed', type=int, default=123,
         help='random seed to use')
     args = parser.parse_args()
@@ -59,26 +63,46 @@ def simulate():
     SCA = desietcimg.sky.SkyCameraAnalysis(nx * binning, ny * binning, binning, CA.flatinvgain)
     SCA.load_fiber_locations(args.fibers)
 
+    if args.nstudy > 1:
+        # Initialize the output file.
+        labels = [label for label in SCA.fibers]
+        simout = open(args.outname, 'w')
+        print(*labels, sep=',', file=simout)
+
     # Calculate the true mean signal rates (elec/sec) in each fiber.
     true_means = init_signals(SCA.fibers, args.max_rate, args.attenuation)
 
-    # Generate a random background image.
-    bg = CA.simulate(exptime=args.exptime, pixbias=args.pixbias,
-                     pixdark=args.pixdark, pixmask=args.pixmask, rng=rng)
+    # Loop over simulated exposures.
+    for i in range(args.nstudy):
 
-    # Add random signals for each fiber.
-    data, true_detected = add_fiber_signals(
-        bg, true_means, SCA, args.exptime, CA.flatinvgain, rng=rng)
+        if args.verbose and i % 100 == 0:
+            print('Simulating {0} / {1}'.format(i + 1, args.nstudy))
 
-    # Perform the measurement.
-    measured = SCA.get_fiber_fluxes(data, args.exptime)
+        # Generate a random background image.
+        bg = CA.simulate(exptime=args.exptime, pixbias=args.pixbias,
+                        pixdark=args.pixdark, pixmask=args.pixmask, rng=rng)
 
-    if args.verbose:
-        for label in SCA.fibers:
-            (xfit, yfit, bgmean, fiber_flux, snr, stamp) = measured[label]
-            print('{0}: mean={1:.1f} det={2:.1f} fit={3:.1f} SNR={4:.1f}'.format(
-                label, true_means[label], true_detected[label], fiber_flux, snr))
+        # Add random signals for each fiber.
+        data, true_detected = add_fiber_signals(
+            bg, true_means, SCA, args.exptime, CA.flatinvgain, rng=rng)
 
-    if args.saveplot:
-        A = plot_sky_camera(SCA)
-        plt.savefig(args.saveplot)
+        # Perform the measurement.
+        measured = SCA.get_fiber_fluxes(data, args.exptime)
+
+        if i == 0 and args.verbose:
+            for label in SCA.fibers:
+                (xfit, yfit, bgmean, fiber_flux, snr, stamp) = measured[label]
+                print('{0}: mean={1:.1f} det={2:.1f} fit={3:.1f} SNR={4:.1f}'.format(
+                    label, true_means[label], true_detected[label], fiber_flux, snr))
+
+        if i == 0 and args.saveplot:
+            A = plot_sky_camera(SCA)
+            plt.savefig(args.saveplot)
+
+        if args.nstudy > 1:
+            # Save the estimated fiber fluxes.
+            values = [measured[label][3] for label in labels]
+            print(*values, sep=',', file=simout)
+
+    if args.nstudy > 1:
+        simout.close()
