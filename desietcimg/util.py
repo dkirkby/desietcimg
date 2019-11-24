7b6ea04d023f102c87217634c2bec0e39c45474c
David Kirkby <dkirkby@uci.edu>
2019-11-24 09:54:17 -0800

    Make util.preprocess more robust

--- desietcimg/util.py
+++ desietcimg/util.py
@@ -63,26 +63,32 @@ def downsample_weighted(D, W, downsampling=4, allow_trim=True):
         raise ValueError('Arrays D, W must have the same shape.')
     if D.ndim != 2:
         raise ValueError('Arrays D, W must be 2D.')
+    if np.any(W < 0):
+        raise ValueError('Array W contains negative values.')
     WD = downsample(D * W, downsampling=downsampling, summary=np.sum, allow_trim=allow_trim)
     W = downsample(W, downsampling=downsampling, summary=np.sum, allow_trim=allow_trim)
     D = np.divide(WD, W, out=np.zeros_like(WD), where=W > 0)
     return D, W
 
 
-def preprocess(D, clip_lo_pct=0.5, clip_hi_pct=99.5, vmin=None, vmax=None, compress=False):
-    """Preprocess 2D array data.
+def preprocess(D, W, nsig_lo=8, nsig_hi=24, vmin=None, vmax=None):
+    """Preprocess weighted 2D array data.
     """
-    if vmin is None and vmax is None:
-        vmin, vmax = np.percentile(D, (clip_lo_pct, clip_hi_pct))
-    elif vmin is None:
-        vmin = np.percentile(D, clip_lo_pct)
-    elif vmax is None:
-        vmax = np.percentile(D, clip_hi_pct)
-    if vmin >= vmax:
-        raise ValueError('Invalid limits vmin >= vmax.')
+    masked = W == 0
+    # Calculate the median unmasked pixel value.
+    median_value = np.median(D[~masked])
+    # Calculate the median non-zero inverse variance.
+    median_ivar = np.median(W[~masked])
+    # Calculate the corresponding pixel sigma.
+    sigma = 1 / np.sqrt(median_ivar)
+    if vmin is None:
+        vmin = median_value - nsig_lo * sigma
+    if vmax is None:
+        vmax = median_value + nsig_hi * sigma
+    # Clip values to [vmin, vmax].
     D = np.clip(D, vmin, vmax)
-    if compress:
-        logging.warning('The compress option is not implemented yet.')
+    # Set masked pixel values to nan so they are not plotted.
+    D[masked] = np.nan
     return D
 
 
