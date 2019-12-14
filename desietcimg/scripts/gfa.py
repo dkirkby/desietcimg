99d050d457913837f841cfa43ff441bb936777d7
David Kirkby <dkirkby@uci.edu>
2019-12-13 16:27:10 -0800

    Implement npool option

--- desietcimg/scripts/gfa.py
+++ desietcimg/scripts/gfa.py
@@ -4,6 +4,7 @@ import os
 import sys
 import argparse
 import warnings
+import multiprocessing
 
 from pathlib import Path
 
@@ -30,41 +31,46 @@ GFA = None
 raw = np.empty((1, 1032, 2248), np.uint32)
 
 
-def process_one(hdus, camera, framepath):
+def process_one(inpath, camera, framepath):
     """Process a single camera of a single exposure.
     """
-    hdr = hdus[camera].read_header()
-    meta = {}
-    for key in 'NIGHT', 'EXPID', 'MJD-OBS', 'EXPTIME', 'GCCDTEMP':
-        if key not in hdr:
-            logging.error('Missing required key {0} for {1}'.format(key, camera))
+    global GFA
+    with fitsio.FITS(str(inpath), mode='r') as hdus:
+        if camera not in hdus:
+            logging.error('Missing HDU {0}'.format(camera))
             return None
-        meta[key] = hdr[key]
-    raw[0] = hdus[camera][:, :]
-    logging.info('Processing {0}'.format(camera))
-    try:
-        GFA.setraw(raw, name=camera)
-    except ValueError as e:
-        logging.error(e)
-        return None
-    GFA.data -= GFA.get_dark_current(meta['GCCDTEMP'], meta['EXPTIME'])
-    if camera.startswith('GUIDE'):
-        GFA.get_psfs()
-        stamps = GFA.psfs
-        result = GFA.psf_stack
-    else:
-        GFA.get_donuts()
-        stamps = GFA.donuts[0] + GFA.donuts[1]
-        result = GFA.donut_stack
-    if framepath is not None:
-        label = '{0} {1} {2:.1f}s'.format(meta['NIGHT'], meta['EXPID'], meta['EXPTIME'])
-        plot_data(GFA.data[0], GFA.ivar[0], downsampling=2, label=label, stamps=stamps, colorhist=True)
-        plt.savefig(framepath / '{0}_{1:08d}.{2}'.format(camera, meta['EXPID'], img_format), quality=80)
-        plt.clf()
-    return result, meta
+        hdr = hdus[camera].read_header()
+        meta = {}
+        for key in 'NIGHT', 'EXPID', 'MJD-OBS', 'EXPTIME', 'GCCDTEMP':
+            if key not in hdr:
+                logging.error('Missing required key {0} for {1}'.format(key, camera))
+                return None
+            meta[key] = hdr[key]
+        raw[0] = hdus[camera][:, :]
+        logging.info('Processing {0}'.format(camera))
+        try:
+            GFA.setraw(raw, name=camera)
+        except ValueError as e:
+            logging.error(e)
+            return None
+        GFA.data -= GFA.get_dark_current(meta['GCCDTEMP'], meta['EXPTIME'])
+        if camera.startswith('GUIDE'):
+            GFA.get_psfs()
+            stamps = GFA.psfs
+            result = GFA.psf_stack
+        else:
+            GFA.get_donuts()
+            stamps = GFA.donuts[0] + GFA.donuts[1]
+            result = GFA.donut_stack
+        if framepath is not None:
+            label = '{0} {1} {2:.1f}s'.format(meta['NIGHT'], meta['EXPID'], meta['EXPTIME'])
+            plot_data(GFA.data[0], GFA.ivar[0], downsampling=2, label=label, stamps=stamps, colorhist=True)
+            plt.savefig(framepath / '{0}_{1:08d}.{2}'.format(camera, meta['EXPID'], img_format), quality=80)
+            plt.clf()
+        return result, meta
 
 
-def process(night, expid, args):
+def process(night, expid, args, pool, pool_timeout=5):
     """Process a single exposure.
     """
     # Locate the exposure path.
@@ -84,18 +90,30 @@ def process(night, expid, args):
     outpath.mkdir(parents=True, exist_ok=True)
     # Open the FITS file to read.
     logging.info('Processing {0}'.format(inpath))
-    hdus = fitsio.FITS(str(inpath), mode='r')
     # Process each camera in the input.
     results = {}
+    framepath = outpath if args.save_frames else None
     for camera in GFA.gfa_names:
-        if camera not in hdus:
-            logging.error('Missing HDU {0}'.format(camera))
-            continue
-        result, meta = process_one(hdus, camera, outpath if args.save_frames else None)
-        if result is None:
-            logging.error('Error processing HDU {0}'.format(camera))
+        if pool is None:
+            result = process_one(inpath, camera, framepath)
+            if result is None:
+                logging.error('Error processing HDU {0}'.format(camera))
+            else:
+                results[camera], meta = result
         else:
-            results[camera] = result
+            results[camera] = pool.apply_async(process_one, (inpath, camera, framepath))
+    if pool:
+        # Collect the pooled results.
+        for camera in results:
+            try:
+                result = results[camera].get(timeout=pool_timeout)
+                if result is None:
+                    logging.error('Error processing HDU {0}'.format(camera))
+                else:
+                    results[camera], meta = result
+            except TimeoutError:
+                logging.error('Timeout waiting for {0} pool result'.format(camera))
+                del results[camera]
     # Save the output FITS file.
     fitspath = outpath / 'stack_{0}.fits'.format(expid)
     with fitsio.FITS(str(fitspath), 'rw', clobber=True) as hdus:
@@ -135,6 +153,8 @@ def gfadiq():
         help='Path to GFA calibration FITS file to use')
     parser.add_argument('--logpath', type=str, metavar='PATH',
         help='Path where logging output should be written')
+    parser.add_argument('--npool', type=int, default=0, metavar='N',
+        help='Number of workers in multiprocessing pool')
     args = parser.parse_args()
 
     # Configure logging.
@@ -193,8 +213,19 @@ def gfadiq():
     global GFA
     GFA = GFACamera(calib_name=args.calibpath)
 
+    if args.npool > 0:
+        # Initialize multiprocessing.
+        context = multiprocessing.get_context(method='fork')
+        if args.npool > context.cpu_count():
+            args.npool = context.cpu_count()
+            log.warning('Reducing --npool to {0}'.format(args.npool))
+        pool = context.Pool(processes=args.npool)
+        logging.info('Initialized multiprocessing pool with {0} workers'.format(args.npool))
+    else:
+        pool = None
+
     if args.night is None or args.expid is None:
         print('Must specify night and expid for now.')
         sys.exit(-1)
 
-    process(args.night, args.expid, args)
+    process(args.night, args.expid, args, pool)
